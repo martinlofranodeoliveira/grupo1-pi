53478b8e489f1ec80cc3ef541cc8cc246ed59b5f
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-09-09 14:08:18 -0300

    fix: app.js scripts

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-document.addEventListener('load', function() {
+// Galeria de imagens modal
+function onClick(element) {
+  
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
 
-    
 
-});
+// Alterne entre mostrar e ocultar a barra lateral ao clicar no ícone do menu
+var mySidebar = document.getElementById("mySidebar");
+
+function consult_open() {
+    if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    } else {
+    mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button 
+function consult_close() {
+    mySidebar.style.display = "none";
+
+}
