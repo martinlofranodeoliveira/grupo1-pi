379a4294fdf21d8ea77779c2f1d352856767b765
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-10-02 16:53:04 -0300

    feat: Rota e Controller Carrinho Martin

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const routerLoginUsuario = require('./routes/loginUsuario')
 const routerLoginParceiro = require('./routes/loginParceiro.js')
 const routerProdutos = require('./routes/produtos.js')
 const routerDescricaoProduto = require('./routes/descricaoProduto.js')
+const routerCarrinho = require('./routes/carrinho.js')
 
 app.set('view engine', 'ejs')
 
@@ -15,6 +16,7 @@ app.use('/', routerLoginUsuario)
 app.use('/', routerLoginParceiro)
 app.use('/', routerProdutos)
 app.use('/', routerDescricaoProduto)
+app.use('/', routerCarrinho)
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.listen(3000, () => {console.log("Servidor rodando na porta 3000")}) 
