4f3b3b6c63f87b2b309e3589cca3693d6e9af39c
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-10-03 20:38:15 -0300

    admin Italo

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,1 +1,252 @@
-let userController = new UserController()
+class Produto {
+
+    constructor() {
+        this.id = 1;
+        this.arryaProdutos = [];
+    }
+
+    salvar() {
+        let produto = this.lerDados();
+
+        if (this.validaCampos(produto)) {
+            this.adicionar(produto)
+        }
+
+        this.listaTabela()
+        this.cancelar()
+
+    }
+
+    listaTabela() {
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+
+        for (let i = 0; i < this.arryaProdutos.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produtos = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+
+            td_id.innerText = this.arryaProdutos[i].id;
+            td_produtos.innerText = this.arryaProdutos[i].nomeProduto;
+            td_valor.innerText = this.arryaProdutos[i].valorProduto;
+
+
+            td_id.classList.add('center')
+
+            let imgEdit = document.createElement('img')
+            imgEdit.src = 'images/edit.svg'
+
+            let imgDelete = document.createElement('img')
+            imgDelete.src = 'images/delete.svg'
+            imgDelete.setAttribute('onclick', 'produto.deletar('+this.arryaProdutos[i].id +')')
+
+            td_acoes.appendChild(imgEdit)
+            td_acoes.appendChild(imgDelete)
+        }
+    }
+
+    adicionar(produto) {
+        this.arryaProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados() {
+        let produto = {}
+
+        produto.id = this.id
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.valorProduto = document.getElementById('valor').value;
+        produto.DescriçãoProduto = document.getElementById('Descrição').value;
+        return produto;
+    }
+
+
+    validaCampos(produto) {
+        let mensagem = '';
+
+        if (produto.nomeProduto == '') {
+            mensagem += '- informe o nome do produto \n'
+        }
+
+        if (produto.valorProduto == '') {
+            mensagem += '- informe o valor do produto \n'
+        }
+
+        if (produto.DescriçãoProduto == '') {
+            mensagem += '- informe o Descrição do produto \n'
+        }
+
+        if (mensagem != '') {
+            alert(mensagem)
+            return false
+
+        }
+
+
+        return true
+    }
+
+
+    cancelar() {
+        document.getElementById('produto').value = '';
+        document.getElementById('valor').value = '';
+        document.getElementById('Descrição').value = '';
+    }
+
+    deletar(id) {
+
+        let tbody = document.getElementById('tbody');
+
+        for(let i = 0 ; i < this.arryaProdutos.length ; i++) {
+            if(this.arryaProdutos[i].id == id){
+                this.arryaProdutos.slice(i,1)
+                tbody.deleteRow(i);
+            }
+        }
+    }
+
+}
+
+
+
+var produto = new Produto();
+
+
+
+/* ocutar */
+
+
+function mostrar(id) {
+    if (document.getElementById(id).style.display == 'block') {
+      document.getElementById(id).style.display = 'none';
+    } else {
+      document.getElementById(id).style.display = 'block'; 
+    }        
+  }
+
+
+
+/* admin */
+
+  class Admin {
+
+    constructor() {
+        this.id = 1;
+        this.arryaAdmin = [];
+    }
+
+    salvar() {
+        let admin = this.lerDados();
+
+        if (this.validaCampos(admin)) {
+            this.adicionar(admin)
+        }
+
+        this.listaTabela()
+        this.cancelar()
+
+    }
+
+    listaTabela() {
+        let tbodyAdm = document.getElementById('tbodyAdmin');
+        tbodyAdm.innerText = '';
+
+        for (let i = 0; i < this.arryaAdmin.length; i++) {
+            let tr = tbodyAdm.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_nome = tr.insertCell();
+            let td_email= tr.insertCell();
+            let td_telefone= tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+
+            td_id.innerText = this.arryaAdmin[i].id;
+            td_nome.innerText = this.arryaAdmin[i].nome;
+            td_email.innerText = this.arryaAdmin[i].email;
+            td_telefone.innerText = this.arryaAdmin[i].telefone;
+
+
+            td_id.classList.add('center')
+
+            let imgEditAdm = document.createElement('img')
+            imgEditAdm.src = 'images/edit.svg'
+
+            let imgDeleteAdm = document.createElement('img')
+            imgDeleteAdm.src = 'images/delete.svg'
+            imgDeleteAdm.setAttribute('onclick', 'admin.deletar('+this.arryaAdmin[i].id +')')
+
+            td_acoes.appendChild(imgEditAdm)
+            td_acoes.appendChild(imgDeleteAdm)
+        }
+    }
+
+    adicionar(admin) {
+        this.arryaAdmin.push(admin);
+        this.id++;
+    }
+
+    lerDados() {
+        let admin = {}
+
+        admin.id = this.id
+        admin.nome = document.getElementById('nome').value;
+        admin.email= document.getElementById('email').value;
+        admin.telefone = document.getElementById('telefone').value;
+        return admin;
+    }
+
+
+    validaCampos(admin) {
+        let mensagem = '';
+
+        if (admin.nome == '') {
+            mensagem += '- informe o nome \n'
+        }
+
+        if (admin.email == '') {
+            mensagem += '- informe o email \n'
+        }
+
+        if (admin.telefone == '') {
+            mensagem += '- informe o telefone \n'
+        }
+
+        if (mensagem != '') {
+            alert(mensagem)
+            return false
+
+        }
+
+
+        return true
+    }
+
+
+    cancelar() {
+        document.getElementById('nome').value= '';
+        document.getElementById('email').value = '';
+        document.getElementById('telefone').value = '';
+    }
+
+    deletar(id) {
+
+        let tbody = document.getElementById('tbodyAdmin');
+
+        for(let i = 0 ; i < this.arryaAdmin.length ; i++) {
+            if(this.arryaAdmin[i].id == id){
+                this.arryaAdmin.slice(i,1)
+                tbody.deleteRow(i);
+            }
+        }
+    }
+
+}
+
+
+
+var admin = new Admin();
