62e6d21a2a778c362dda29f2b8df18675a3819fc
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-10-03 22:46:17 -0300

    feat tentando fazer funcionar js login  usuario

--- public/js/login-usuario.js
+++ public/js/login-usuario.js
@@ -1,10 +1,35 @@
+let fs = require('fs')
+let usuarios = require ("./database/usuarios.json");
+let usuarioStr = JSON.stringify(usuarios);
+
 var gmailCadastro = document.getElementById("gmail-cadrastro");
 var senhaCadastro = document.getElementById("senha-cadrastro");
 var nomeCadastro = document.getElementById("nome");
 
 var  btnEntrar = document.getElementById("Ientra-cadrastro");
 
+function adicionarUsuario(usuarios, dadosUsuario){
+  let usuarioExistente = false
+  let newData = ""
 
+  for (let i = 0; i < usuarios['usuarios'].length;i++){        
+      if (dadosUsuario['codigo'] === usuarios['usuarios'][i]['codigo']){
+          usuarioExistente = true           
+      }
+  }
+  if (!usuarioExistente){
+      usuarios['usuarios'].push(dadosUsuario)
+      newData = JSON.stringify(usuarios)
+      fs.writeFile ('./database/usuarios.json', newData, err => {
+          // Verificação de erros
+          (err) ? console.log("Erro ao inserir os dados") : console.log ("Novos dados adicionados");        
+      });
+      return JSON.parse(newData)
+  }else{
+      console.log("Usuario já existente")
+      return usuarios
+  }
+}
 
 
 function validaCadastro(){
@@ -18,7 +43,7 @@ function validaCadastro(){
   var usuario = {
         nome: nomeCadastro.value,
         email: gmailCadastro.value,
-        senha: gmailCadastro.value
+        senha: senhaCadastro.value
   };
   usuarios.push(usuario);
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
