a6e2eb17c33e710c9e65dacf95841b2dfd571b4d
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-10-04 19:50:22 -0300

    js login usuario

--- public/js/login-usuario.js
+++ public/js/login-usuario.js
@@ -8,6 +8,76 @@ var nomeCadastro = document.getElementById("nome");
 
 var  btnEntrar = document.getElementById("Ientra-cadrastro");
 
+function validaCadastro(){
+  var usuarios = localStorage.getItem("usuarios");
+  usuarios = JSON.parse(usuarios);
+
+  if(usuarios == null){
+    usuarios = [];
+  }
+
+  var usuario = {
+        nome: nomeCadastro.value,
+        email: gmailCadastro.value,
+        senha: senhaCadastro.value
+  };
+  usuarios.push(usuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  alert("Usuário criado com sucesso");
+}
+
+btnEntrar.addEventListener("click", validaCadastro);
+
+
+let usuarios = [
+  {
+      id: 1,
+      nome: 'Martin',
+      email: 'genesisrecordshouse@gmail.com'
+  },
+  {
+      id: 2,
+      nome: 'Diogenes',
+      email: 'diogenes@digitalhouse.com.br'
+  },
+  {
+      id: 3,
+      nome: 'Jonatas',
+      email: 'jonatas@digitalhouse.com.br'
+  }
+]
+
+
+app.use(express.json())
+app.get('/login-usuario', (req, res) => {
+  res.status(200).send(usuarios)
+})
+
+app.post('/login-usuario', (req, res) => {
+  usuarios.push(req.body)
+  res.status(201).send(usuarios)
+})
+
+app.put('/login-usuario/:id', (req, res) => {
+  let usuario = usuarios.find(usr => usr.id == req.params.id)
+
+  if (!usuario){
+      return res.status(400).json({error: 'Usuario não encontrado.' });
+  }
+
+  usuario.nome = req.body.nome
+  usuario.email = req.body.email
+  usuario.senha = req.body.senha
+  res.send(usuarios)
+})
+
+app.delete('/usuarios/:id', (req, res) => {
+  let usuario = usuarios.find(usr => usr.id == req.params.id)
+  usuarios.splice(usuarios.indexOf(usuario), 1)
+  res.status(204).send(usuarios)
+})
+
+
 function adicionarUsuario(usuarios, dadosUsuario){
   let usuarioExistente = false
   let newData = ""
@@ -31,23 +101,3 @@ function adicionarUsuario(usuarios, dadosUsuario){
   }
 }
 
-
-function validaCadastro(){
-  var usuarios = localStorage.getItem("usuarios");
-  usuarios = JSON.parse(usuarios);
-
-  if(usuarios == null){
-    usuarios = [];
-  }
-
-  var usuario = {
-        nome: nomeCadastro.value,
-        email: gmailCadastro.value,
-        senha: senhaCadastro.value
-  };
-  usuarios.push(usuario);
-  localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  alert("Usuário criado com sucesso");
-}
-
-btnEntrar.addEventListener("click", validaCadastro);
