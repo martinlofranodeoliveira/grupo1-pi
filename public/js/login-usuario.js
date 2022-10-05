ca14c6dcae5e25e4f0679876ff96d70edb822dc4
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-10-04 21:55:39 -0300

    js login usuario

--- public/js/login-usuario.js
+++ public/js/login-usuario.js
@@ -1,53 +1,54 @@
-let fs = require('fs')
-let usuarios = require ("./database/usuarios.json");
-let usuarioStr = JSON.stringify(usuarios);
-
-var gmailCadastro = document.getElementById("gmail-cadrastro");
-var senhaCadastro = document.getElementById("senha-cadrastro");
-var nomeCadastro = document.getElementById("nome");
-
-var  btnEntrar = document.getElementById("Ientra-cadrastro");
-
 function validaCadastro(){
-  var usuarios = localStorage.getItem("usuarios");
-  usuarios = JSON.parse(usuarios);
-
-  if(usuarios == null){
-    usuarios = [];
+  let fs = require('fs')
+  let usuarios = require ("./database/usuarios.json");
+  let usuarioStr = JSON.stringify(usuarios);
+  
+  function adicionarUsuario(usuarios, dadosUsuario){
+    var usuarios = localStorage.getItem("usuarios");
+    usuarios = JSON.parse(usuarios);
+  
+    if(usuarios == null){
+      usuarios = [];
+    }
+  
+    var usuario = {
+          nome: document.getElementById("nome").value,
+          email: document.getElementById("gmail-cadrastro").value,
+          senha: document.getElementById("senha-cadrastro").value
+    };
+    usuarios.push(usuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    alert("Usuário criado com sucesso");
   }
-
-  var usuario = {
-        nome: nomeCadastro.value,
-        email: gmailCadastro.value,
-        senha: senhaCadastro.value
-  };
-  usuarios.push(usuario);
-  localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  alert("Usuário criado com sucesso");
-}
-
-btnEntrar.addEventListener("click", validaCadastro);
-
-
-let usuarios = [
-  {
-      id: 1,
-      nome: 'Martin',
-      email: 'genesisrecordshouse@gmail.com'
-  },
-  {
-      id: 2,
-      nome: 'Diogenes',
-      email: 'diogenes@digitalhouse.com.br'
-  },
-  {
-      id: 3,
-      nome: 'Jonatas',
-      email: 'jonatas@digitalhouse.com.br'
+  
+  btnEntrar.addEventListener("click", validaCadastro);
   }
-]
-
-
+  
+    function validaCadastro(){
+      let usuarioExistente = false
+      let newData = ""
+    
+      for (let i = 0; i < usuarios['usuarios'].length;i++){        
+          if (dadosUsuario['codigo'] === usuarios['usuarios'][i]['codigo']){
+              usuarioExistente = true           
+          }
+      }
+      if (!usuarioExistente){
+          usuarios['usuarios'].push(dadosUsuario)
+          newData = JSON.stringify(usuarios)
+          fs.writeFile ('./database/usuarios.json', newData, err => {
+              // Verificação de erros
+              (err) ? console.log("Erro ao inserir os dados") : console.log ("Novos dados adicionados");        
+          });
+          return JSON.parse(newData)
+      }else{
+          console.log("Usuario já existente")
+          return usuarios
+      }
+    }
+    
+  
+/* 
 app.use(express.json())
 app.get('/login-usuario', (req, res) => {
   res.status(200).send(usuarios)
@@ -77,27 +78,4 @@ app.delete('/usuarios/:id', (req, res) => {
   res.status(204).send(usuarios)
 })
 
-
-function adicionarUsuario(usuarios, dadosUsuario){
-  let usuarioExistente = false
-  let newData = ""
-
-  for (let i = 0; i < usuarios['usuarios'].length;i++){        
-      if (dadosUsuario['codigo'] === usuarios['usuarios'][i]['codigo']){
-          usuarioExistente = true           
-      }
-  }
-  if (!usuarioExistente){
-      usuarios['usuarios'].push(dadosUsuario)
-      newData = JSON.stringify(usuarios)
-      fs.writeFile ('./database/usuarios.json', newData, err => {
-          // Verificação de erros
-          (err) ? console.log("Erro ao inserir os dados") : console.log ("Novos dados adicionados");        
-      });
-      return JSON.parse(newData)
-  }else{
-      console.log("Usuario já existente")
-      return usuarios
-  }
-}
-
+ */
