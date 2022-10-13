e5fea025904904fd38ae909841a210225e57920d
Demetrius Macêdo <macedo.demetrius@gmail.com>
2022-10-13 16:04:16 -0300

    criada insersção dinamica de produtos na tela de produtos
    ainda não está funcionando corretamente devido ao css

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -1,10 +1,13 @@
+const fs = require('fs');
 const path = require("path");
+
+const produtosFilePath = path.join(__dirname, '../database/products.json');
+const products = JSON.parse(fs.readFileSync(produtosFilePath, 'utf-8'));
 
 const produtosController = {
     listaProdutos: (req, res) => {
-        
         res.render("produtos", {
-            title: 'Produtos',
+            products,
         });
     },
 };
