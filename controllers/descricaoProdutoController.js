3511b789ac228d8467e422b7fde6662feb30a84c
Martin Lofrano de Oliveira <genesisrecordshouse@gmail.com>
2022-11-06 16:02:46 -0300

    feat melhoria html css pagina produtos Martin

--- controllers/descricaoProdutoController.js
+++ controllers/descricaoProdutoController.js
@@ -7,8 +7,9 @@ const products = JSON.parse(fs.readFileSync(produtosFilePath, 'utf-8'));
 const descricaoProdutoController = {
     descricaoProduto: (req, res) => {
         let id = req.params.id;
-        let descricaoProduto = products.find(product => product.id ==id);
+        let descricaoProduto = products.find(product => product.id == id);
         res.render('descricao-produto', {descricaoProduto});
     },
 };
 module.exports = descricaoProdutoController
+
